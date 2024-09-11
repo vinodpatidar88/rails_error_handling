d22dfc541b9494108599ab513b04881955381653
vinodpatidar88 <vinodpatidar9669@gmail.com>
2024-09-11 10:10:50 +0530

    login token

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,9 +3,13 @@ class ApiController < ActionController::API
   rescue_from ApiExceptions::BaseException, with: :render_error_response
   attr_accessor :current_user
 
+  def render_error_response(error)
+    render json: error.to_json, status: error.code
+  end
+
   def authenticate
-    token = LoginToken.find_by(token: request.header[:token])
-    if token.blank?
+    token = LoginToken.find_by(token: request.headers[:token])
+    if token.present?
        token.user
     else
        raise ApiExceptions::UserError::AuthenticationError.new
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
     has_many :login_tokens
+    enum status: %i[inactive active]
+    validates :email, uniqueness: true, presence: true
+    validates :mobile_number, uniqueness: true, presence: true
 end
